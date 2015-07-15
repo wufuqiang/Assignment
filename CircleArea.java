78135325f23b6d3aad8e183ae6c173f35443f29f
tushengsh <826418696@qq.com>
2015-07-15 08:52:53 +0800

    the area of the circle

---
+++ CircleArea.java
@@ -0,0 +1,16 @@
+
+public class CircleArea {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("The area of the circle is:"+GetArea(10));
+
+	}
+
+	private static double GetArea(double size) {
+		// TODO Auto-generated method stub
+		return 3.14*size*size;
+	}
+
+}
