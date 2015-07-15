3cc1d56c3121137989f85c8d299536549dd53708
tushengsh <826418696@qq.com>
2015-07-15 17:27:03 +0800

    average score used while do

---
+++ AverageSourceWhileDo.java
@@ -0,0 +1,24 @@
+package AvergeSource;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class AverageSourceWhileDo {
+
+public static void main(String[] args) {
+   int sum = 0;
+   int score = 0;
+   int count= 0;
+   Scanner sc = new Scanner(System.in);
+   System.out.println("please input score and end with #");
+   while (score!= '#'){
+	   System.out.print("please input score£º");
+       score = sc.nextInt();
+       sum += score;
+       count ++;
+      } 
+   
+      System.out.println( + --count + "students' total score:" + sum + "£¬and average score :23" + (double)sum / count);
+   }
+
+}
