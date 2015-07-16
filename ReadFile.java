9a494b908122b7cf05e6d266763c46d0d8aece33
tushengsh <826418696@qq.com>
2015-07-16 14:27:49 +0800

    read a large file

---
+++ ReadFile.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
+
+public class ReadFile {
+	public static void main(String args[]){
+	String s="";
+	try{
+	FileReader rf=new FileReader("E:\\x.txt");
+	BufferedReader brf=new BufferedReader(rf);
+	String rs="";	
+while((rs=brf.readLine())!=null)
+	s=s+rs+"\n";
+ //   brf.close;
+    rf.close();
+	}
+	catch(IOException e){
+		System.out.println(e);
+		}
+	System.out.print(s);
+	}
+}
+
